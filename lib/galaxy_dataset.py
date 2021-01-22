4fcb1c415f7be8f799d766e55c278c83fcbe3faa
hoaiht <hthoai1006@gmail.com>
2021-01-22 19:05:34 +0700

    MOD dataset class

--- lib/galaxy_dataset.py
+++ lib/galaxy_dataset.py
@@ -1,15 +1,11 @@
 import os
 import logging
-
 import pandas as pd
-import numpy as np
 from PIL import Image
-
 import imgaug.augmenters as iaa
 import torch
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
-from torchvision import transforms
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import ToTensor
 
 
 SPLIT_FILES = {
@@ -20,17 +16,17 @@ SPLIT_FILES = {
 IMAGENET_MEAN = np.array([0.485, 0.456, 0.406])
 IMAGENET_STD = np.array([0.229, 0.224, 0.225])
 
-class GalaxyDataset(object):
+class GalaxyDataset(Dataset):
     
     def __init__(self,
                  split='train',
                  root=None,
                  augmentations=None,
-                 transforms=None,
+                 normalize=False,
                  aug_chance=1.0,
                  img_size=(424, 424)):
         super(GalaxyDataset, self).__init__()
-        self.transforms = transforms
+        self.normalize = normalize
         self.img_h, self.img_w = img_size
         self.logger = logging.getLogger(__name__)
         if root is None:
@@ -46,11 +42,9 @@ class GalaxyDataset(object):
                              for aug in augmentations]  # add augmentation
         else:
             augmentations = []
-        transformations = iaa.Sequential([
-            iaa.Resize({'height': self.img_h, 'width': self.img_w})])
-        self.transform = iaa.Sequential([
-            iaa.Sometimes(then_list=augmentations, p=aug_chance),
-            transformations])
+        transformations = iaa.Sequential([Resize({'height': self.img_h, 'width': self.img_w})])
+        self.to_tensor = ToTensor()
+        self.transform = iaa.Sequential([iaa.Sometimes(then_list=augmentations, p=aug_chance), transformations])
         
     def __getitem__(self, idx):
         img_path = self.img_paths[idx]
@@ -64,21 +58,4 @@ class GalaxyDataset(object):
     
     def __len__(self):
         return len(self.img_paths)
-    
-class GalaxyDataModule(pl.LightningDataModule):
-    def setup(self, stage):
-        # transforms for images
-        transform = transforms.Compose([transforms.ToTensor, 
-                                      transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD)])
-        self.train = GalaxyDataset(split=SPLIT_FILES['train'], root='data/', transforms=transform)
-        self.val = GalaxyDataset(split=SPLIT_FILES['val'], root='data/', transforms=transform)
-        self.test = GalaxyDataset(split=SPLIT_FILES['test'], root='data/', transforms=transform)
         
-    def train_dataloader(self):
-        return DataLoader(self.train, batch_size=8)
-
-    def val_dataloader(self):
-        return DataLoader(self.val, batch_size=8)
-    
-    def test_dataloader(self):
-        return DataLoader(self.test, batch_size=8)
