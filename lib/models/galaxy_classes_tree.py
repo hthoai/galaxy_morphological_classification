a0050a23392d3a01eca2d07cba3efa8bcdf21067
Mai Hoàng Lân (VNCDLL-CTÐTKSAI) <lanmh1@vingroup.net>
2021-01-23 17:27:11 +0700

    CHANGE to graphviz

--- lib/models/galaxy_classes_tree.py
+++ lib/models/galaxy_classes_tree.py
@@ -1,30 +1,42 @@
-from anytree import Node, RenderTree
+from graphviz import Digraph 
 
-# decision tree of 11 main galaxy classes (super classes)
+# Decision tree of 11 main galaxy classes (super classes)
 
-# initialize nodes
-super_class1 = Node('super_class1')
-super_class2 = Node('super_class2')
-super_class3 = Node('super_class3')
-super_class4 = Node('super_class4')
-super_class5 = Node('super_class5')
-super_class6 = Node('super_class6')
-super_class7 = Node('super_class7')
-super_class8 = Node('super_class8')
-super_class9 = Node('super_class9')
-super_class10 = Node('super_class10')
-super_class11 = Node('super_class11')
+# initialize graph project
+galaxy_classes_tree = Digraph(comment='Galaxy Classification Decision Tree')
 
-# build tree
-super_class1.children = [super_class2, super_class7]
-super_class2.children = [super_class3, super_class9]
-super_class3.children = [super_class4]
-super_class4.children = [super_class5, super_class10]
-super_class5.children = [super_class6]
-super_class6.children = [super_class8]
-super_class7.children = [super_class6]
-super_class9.children = []
-super_class10.children = [super_class11]
-super_class11.children = [super_class5]
+# initialize nodes 
+galaxy_classes_tree.node('class_1', label='Class 1')
+galaxy_classes_tree.node('class_2', label='Class 2')
+galaxy_classes_tree.node('class_3', label='Class 3')
+galaxy_classes_tree.node('class_4', label='Class 4')
+galaxy_classes_tree.node('class_5', label='Class 5')
+galaxy_classes_tree.node('class_6', label='Class 6')
+galaxy_classes_tree.node('class_7', label='Class 7')
 
-print(RenderTree(super_class1))
+# connect nodes (direct graph with no weights)
+galaxy_classes_tree.edge('class_1', 'class_7', label='1.1')
+galaxy_classes_tree.edge('class_1', 'class_2', label='1.2')
+
+galaxy_classes_tree.edge('class_2', 'class_9', label='2.1')
+galaxy_classes_tree.edge('class_2', 'class_3', label='2.2')
+
+galaxy_classes_tree.edge('class_3', 'class_4', label='3.1, 3.2')
+
+galaxy_classes_tree.edge('class_4', 'class_10', label='4.1')
+galaxy_classes_tree.edge('class_4', 'class_5', label='4.2')
+
+galaxy_classes_tree.edge('class_5', 'class_6', label='5.1, 5.2, 5.3, 5.4')
+
+galaxy_classes_tree.edge('class_6', 'class_8', label='6.1')
+
+galaxy_classes_tree.edge('class_7', 'class_6', label='7.1, 7.2, 7.3')
+
+galaxy_classes_tree.edge('class_9', 'class_6', label='9.1, 9.2, 9.3')
+
+galaxy_classes_tree.edge('class_10', 'class_11', label='10.1, 10.2, 10.3')
+
+galaxy_classes_tree.edge('class_11', 'class_5', label='11.1, 11.2, 11.3, 11.4, 11.5')
+
+# render tree
+galaxy_classes_tree.render(view=True)
