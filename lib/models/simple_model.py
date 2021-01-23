cc4f79c0682a9e1c8758780301bae3c9be1f92e9
Mai Hoàng Lân (VNCDLL-CTÐTKSAI) <lanmh1@vingroup.net>
2021-01-23 11:38:20 +0700

    ADD VGG-16 base

--- lib/models/simple_model.py
+++ lib/models/simple_model.py
@@ -1,20 +1,27 @@
-from torch.nn import Conv2d, MaxPool2d, Linear, ReLU, Module
+from torch.nn import Conv2d, MaxPool2d, Linear, Module
+from torch.nn import ReLU, Softmax
 import torch.nn.functional as F
 import numpy as np
 import pandas as pd
+import torchvision.models as models
+import torchsummary as summary
+import torch
 
 class SimpleModel(Module):
     def __init__(self):
-        self.conv1 = Conv2d(in_channels=3, out_channels=32, kernel_size=3)
-        self.maxpool1 = MaxPool2d(kernel_size=3)
+        super().__init__()
+        # use pre-trained base
+        self.vgg16 = models.vgg16(pretrained=True)
+        self.alexnet = models.alexnet(pretrained=True)
+        self.resnet18 = models.resnet18(pretrained=True)
+        # self-constructed head
         self.dense1 = Linear(in_features=3, out_features=37)
+        # activations
+        self.Softmax = Softmax()
         self.ReLu = ReLU()
 
     def forward(self, input):
-        input = self.conv1(input)
-        input = self.maxpool1(input)
+        # assemble model
+        input = self.vgg16(input)
         input = self.dense1(input)
-        input = self.ReLu(input)
-    
-    
-        
+        input = self.Softmax(input)
