ae5ade7ca5fbeff07fa917780ea904f3427d0d74
hthoai <hthoai1006@gmail.com>
2021-01-28 14:58:36 +0000

    MOD transform: crop, resize

--- lib/datasets/galaxy_dataset.py
+++ lib/datasets/galaxy_dataset.py
@@ -1,9 +1,11 @@
 import os
 import logging
+from imgaug.augmenters.size import CropToFixedSize
 import pandas as pd
 import numpy as np
 
-from PIL import Image
+# from PIL import Image
+import cv2
 import imgaug.augmenters as iaa
 
 import torch
@@ -13,13 +15,11 @@ from torchvision.transforms import ToTensor
 
 SPLIT_FILES = {
     'train': 'train.csv',
-    'val': 'val.csv',
-    'test': 'test.csv'
+    'val': 'val.csv'
 }
 # SPLIT_FILES = {
 #     'train': 'train_debug.csv',
 #     'val': 'val_debug.csv',
-#     'test': 'test_debug.csv'
 # }
 IMAGE_EXT = '.jpg'
 IMAGE_FOLDER = 'images_training_rev1'
@@ -34,7 +34,7 @@ class GalaxyDataset(Dataset):
                  augmentations=None,
                  normalize=False,
                  aug_chance=1.0,
-                 img_size=(424, 424)):
+                 img_size=(69, 69)):
         super(GalaxyDataset, self).__init__()
         self.split = split
         self.normalize = normalize
@@ -53,16 +53,18 @@ class GalaxyDataset(Dataset):
         #                      for aug in augmentations]  # add augmentation
         # else:
         #     augmentations = []
-        # transformations = iaa.Sequential([iaa.Resize({'height': self.img_h, 'width': self.img_w})])
+        transformations = iaa.Sequential([iaa.CropToFixedSize(207, 207), iaa.Resize({'height': self.img_h, 'width': self.img_w})])
         self.to_tensor = ToTensor()
+        self.transform = iaa.Sequential([transformations])
         # self.transform = iaa.Sequential([iaa.Sometimes(then_list=augmentations, p=aug_chance), transformations])
         
     def __getitem__(self, idx):
         img_path = self.img_paths[idx]
-        img = Image.open(img_path + IMAGE_EXT).convert('RGB')
+        img = cv2.imread(img_path + IMAGE_EXT)
+        # img = Image.open(img_path + IMAGE_EXT).convert('RGB')
         if self.normalize:
             img = (img - IMAGENET_MEAN) / IMAGENET_STD
-        # img = self.transform(image=img.copy())
+        img = self.transform(image=img.copy())
         img = self.to_tensor(img)
         target = torch.tensor(self.targets[idx], dtype=torch.float)
         return img, target
@@ -71,7 +73,7 @@ class GalaxyDataset(Dataset):
         return len(self.img_paths)
     
 # galaxy = GalaxyDataset(root='datasets')
-# # print(galaxy.__getitem__(0))
+# print(galaxy.__getitem__(0)[0].shape)
 # train_loader = torch.utils.data.DataLoader(dataset=galaxy,
 #                                            batch_size=8,
 #                                            shuffle=True,
--- lib/datasets/galaxy_pred_dataset.py
+++ lib/datasets/galaxy_pred_dataset.py
@@ -4,7 +4,8 @@ import logging
 import pandas as pd
 import numpy as np
 
-from PIL import Image
+# from PIL import Image
+import cv2
 import imgaug.augmenters as iaa
 
 import torch
@@ -20,27 +21,30 @@ IMAGENET_STD = np.array([0.229, 0.224, 0.225])
 class GalaxyPredictDataset(Dataset):
     
     def __init__(self,
+                 split='pred',
                  root=None,
                  normalize=False,
                  img_size=(424, 424)):
         super(GalaxyPredictDataset, self).__init__()
         self.normalize = normalize
-        self.split = 'pred'
+        self.split = split
         self.img_h, self.img_w = img_size
         self.logger = logging.getLogger(__name__)
         if root is None:
             raise Exception('Please specify the root directory.')
         self.img_paths = glob(root + '/' + IMAGE_TEST_FOLDER + '/*' + IMAGE_EXT)
-        self.img_paths = self.img_paths[:160]
+        # self.img_paths = self.img_paths[:160]
+        transformations = iaa.Sequential([iaa.CropToFixedSize(207, 207), iaa.Resize({'height': self.img_h, 'width': self.img_w})])
         self.to_tensor = ToTensor()
-        # self.transform = iaa.Sequential([iaa.Sometimes(then_list=augmentations, p=aug_chance), transformations])
-        
+        self.transform = iaa.Sequential([transformations])
+
     def __getitem__(self, idx):
         img_path = self.img_paths[idx]
-        img = Image.open(img_path).convert('RGB')
+        # img = Image.open(img_path).convert('RGB')
+        img = cv2.imread(img_path)
         if self.normalize:
             img = (img - IMAGENET_MEAN) / IMAGENET_STD
-        # img = self.transform(image=img.copy())
+        img = self.transform(image=img.copy())
         img = self.to_tensor(img)
         img_id = img_path.split('/')[-1].split('.')[0]
         return img_id, img
@@ -49,7 +53,7 @@ class GalaxyPredictDataset(Dataset):
         return len(self.img_paths)
     
 # galaxy = GalaxyPredictDataset(root='datasets')
-# print(galaxy.__getitem__(0)[0])
+# print(galaxy.__getitem__(0)[1].shape)
 # train_loader = torch.utils.data.DataLoader(dataset=galaxy,
 #                                            batch_size=8,
 #                                            shuffle=True,
