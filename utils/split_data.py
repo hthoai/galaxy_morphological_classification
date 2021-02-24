34f57eca311c6fd8681587816eff117f396769a0
hoaiht <hthoai1006@gmail.com>
2021-02-24 04:49:50 +0000

    MOD train val set only

--- utils/split_data.py
+++ utils/split_data.py
@@ -7,10 +7,10 @@ RANDOM_STATE = 1412
 df = pd.read_csv('datasets/training_solutions_rev1.csv')
 print(f'df shape: {df.shape}')
 # Split
-train, test = train_test_split(df, test_size=0.2, random_state=RANDOM_STATE)
-train, val = train_test_split(train, test_size=0.2, random_state=RANDOM_STATE)
-print(f'train shape: {train.shape}\tval shape: {val.shape}\ttest shape: {test.shape}')
+train, val = train_test_split(df, test_size=0.2, random_state=RANDOM_STATE)
+# train, val = train_test_split(train, test_size=0.2, random_state=RANDOM_STATE)
+print(f'train shape: {train.shape}\tval shape: {val.shape}')
 # Save
 train.to_csv('datasets/train.csv', index=False)
 val.to_csv('datasets/val.csv', index=False)
-test.to_csv('datasets/test.csv', index=False)
+# test.to_csv('datasets/test.csv', index=False)
